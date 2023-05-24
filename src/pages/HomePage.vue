8c690c8002c1436b524af7aaeaeb145d675820ee
Isaac Duff <4mustangid@gmail.com>
2023-05-24 11:37:58 -0600

    int

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <img src="" alt="">
+      <h2>Isaac Duff</h2>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: 50vw;
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
